b495b6e050ccfeb728df87eb704b6f8f235e3519
Neil Zhao <neilzhao200@gmail.com>
2022-01-16 12:09:49 -0500

    chore: day16 bootstrap

---
+++ day016/main.py
@@ -0,0 +1,11 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+
+def start_coffee_machine():
+    pass
+
+
+if __name__ == "__main__":
+    start_coffee_machine();
